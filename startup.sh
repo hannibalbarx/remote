2e538721c7f57ca1385d8c2486da7fefca7ac3e9
hannibalbarx <vvarkey@riseup.net>
2014-11-14 01:17:22 +0530

    who?

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
-echo "hi it's `date`">/home/laserwolf/hi
+echo "hi it's `date`">/home/$USER/hi
 wget -O /root/attach_volume.py https://raw.githubusercontent.com/hannibalbarx/remote/upwheezy/attach_volume.py
 #/usr/bin/python /root/attach_volume.py
 
 #mount /dev/sdb /mnt/permalearn
 
-#su ec2-user -c "/home/ec2-user/permalearn/depp.sh"
+#su $USER -c "/home/$USER/permalearn/depp.sh"
 
 #comment
