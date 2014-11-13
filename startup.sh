cb2eee81cb92f80770c937905209f1ef4da8c1f9
hannibalbarx <vvarkey@riseup.net>
2014-11-14 00:24:10 +0530

    make working

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
-wget -O /root/attach_volume.py http://pastebin.com/raw.php?i=2URnuebR
-/usr/bin/python /root/attach_volume.py
+echo "hi it's `date`">/home/laserwolf/hi
+wget -O /root/attach_volume.py https://raw.githubusercontent.com/hannibalbarx/remote/upwheezy/attach_volume.py
+#/usr/bin/python /root/attach_volume.py
 
-mount /dev/sdb /mnt/permalearn
+#mount /dev/sdb /mnt/permalearn
 
-su ec2-user -c "/home/ec2-user/permalearn/depp.sh"
+#su ec2-user -c "/home/ec2-user/permalearn/depp.sh"
 
 #comment
